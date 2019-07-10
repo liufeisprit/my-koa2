7cd9e830d8ed8ee1401b20a82d309f0daa1e0658
liufeisprit <649912343@qq.com>
2019-07-11 00:18:01 +0800

    11-1

---
+++ koa2/src/layouts/default.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react'
+import {Menu,Spin} from 'antd'
+import {Link} from 'react-router-dom'
+import navRoutes from './nav'
+const getMenuContent=({path,name})=>{
+    <a href={path?path:'/'} style={{color:'#fff2e8'}}>
+        {name}
+    </a>
+}
+export default class Layout extends Component{
+    constructor(){
+        super(props)
+        this.state={
+            loading:false,
+            tip:'再等一下下嘛'
+        }
+    }
+    componentDidMount() {
+        window.__LOADING__= this.toggleLoading
+    }
+    componentWillMount() {
+        window.__LOADING__= null
+    }
+    matchRouteName=this.props.match
+    ? navRoutes.find(e=>e.name===this.props.match.params.type)
+    ?  navRoutes.find(e=>e.name===this.props.match.params.type).name
+    : '全部'
+    : navRoutes[0].name
+    toggleLoading = (status=false,tip='再等一下下嘛!')=>{
+        this.setState({
+            tip,
+            loading:status
+        })
+    }
+    render() {
+        const {children}=this.props;
+        const {loading , tip}=this.props;
+        return (
+            <div className='flex-form' style={{width:'100%',height:'100%'}}></div>
+            <Menu
+                mode='horizontal'
+                style={{fontSize:12.5,backgroundColor:'#000'}}
+                defaultSelectKeys={this.matchRouteName}
+            >
+                <Menu.Item 
+                    style={{
+                        marginLeft:24,
+                        marginRight:30,
+                        fontSize:18,
+                        textAlign:center,
+                        color:'#fff !important',
+                        float:'left'
+                    }}
+                >
+                    <a href={'/'} className='hover-scale logo-text' style={{
+                        color:'#fff2e8'
+                    }}>预告片</a>
+                    {
+                        navRoutes.map((e,i)=>{
+                            <Menu.Item>
+                            {
+                                getMenuContent({...e})
+                            }
+                            </Menu.Item>
+                        })
+                    }
+                </Menu.Item>
+                <Spin
+                    spinning={loading}
+                    tip={tip}
+                    wrapperClassName='content-spin full'
+                >
+                    {children}
+                </Spin>
+            </Menu>
+        )
+    }
+}
---
+++ koa2/src/layouts/nav.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name:'全部',
+        path:'/'
+    },
+    {
+        name:'s科幻b',
+        path:`/list/${encodeURIComponent('科幻')}`
+    },
+]
